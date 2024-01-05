a245ae2a432126538fdc327f9f260a01f070c974
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-06 00:41:35 +0530

    Update and rename Prac112_Assgn_Q3.java to Prac112.java

--- Prac112.java
+++ Prac112.java
@@ -6,17 +6,17 @@ public class Prac112_Assgn_Q3 {
 	    System.out.println("Enter n");
 		int n = sc.nextInt();
 		int sum=0;
-		int count=1;
+		int count=1;  //1 to n
 		int random;
 		System.out.println("Random numbers generated are: ");
 		do {
-			random = (int)(Math.random()*n+1);
-			System.out.println(random);
+			random = (int)(Math.random()*n+1);    //random values by computer
+			System.out.println(random);	      //adding the random values
 			sum = sum + random;
 			count++;
 		}while(count<=n);
 		
-		int average = sum/n;
+		int average = sum/n;			      //calculating average
 		System.out.println("the average is: "+ average);
 
 	}
